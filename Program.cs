ca2e20aa73bed9e7c71ba53377852b00f6c27b51
e190476 <l.delamarck@student.helmo.be>
2023-10-12 15:57:50 +0200

    Maj Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,56 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using VacancyProAPI;
 using VacancyProAPI.Models;
+using VacancyProAPI.Models.DbModels;
+using VacancyProAPI.Services.UserService;
 
 var builder = WebApplication.CreateBuilder(args);
+var builderConf = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
-// Add services to the container.
 
-builder.Services.AddControllers();
+var configuration = builderConf.Build();
 
+var connectionString = configuration.GetConnectionString("default");
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddHttpContextAccessor();
+// builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+
+// TODO : Remplacer UseSqlServer par la connctionString
 builder.Services.AddDbContext<DatabaseContext>(opt =>
 {
     opt.UseSqlServer("Server=Q200007;Database=Q200007;User=Q200007;Password=0007;");
 });
 
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+ ";
+}).AddEntityFrameworkStores<DatabaseContext>();
 
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:JwtSecret").Value)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,14 +58,23 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    var userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+    var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+    var config = scope.ServiceProvider.GetService<IConfiguration>();
 
+    await Seed.AddDefaultRole(roleManager);
+}
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
